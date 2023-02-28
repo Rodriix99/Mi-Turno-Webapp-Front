feb7cf60e800b3acad3d73993a00de9f0668a463
noeliacossio <noeliacos_8@hotmail.com>
2023-02-28 16:45:49 -0300

    changes

--- src/commons/DropDown.tsx
+++ src/commons/DropDown.tsx
@@ -23,11 +23,11 @@ useEffect(() => {
 
 
   return (
-    <div className="relative w-full lg:max-w-sm">
-      <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+    <div className="relative w-full">
+      <select className="w-full p-2.5 text-gray-500 bg-white border rounded-lg shadow-sm focus:border-indigo-600"
       placeholder="Elegir sucursal">
       {array.length === 0 ? null : 
-      array?.map((option: any, i) => (<option key={i} value={option._id}>{option.name}</option>))}      
+      array?.map((branch: any, i) => (<option key={i} value={branch._id}>{branch.name}</option>))}      
       </select>
     </div>
   );
