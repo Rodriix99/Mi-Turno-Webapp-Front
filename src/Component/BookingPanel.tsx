243aa36b508452a1f7cac93acb5299c3e128b779
Javi970 <javierpaivag@gmail.com>
2023-02-28 11:52:50 -0300

    fix components

--- src/Component/BookingPanel.tsx
+++ src/Component/BookingPanel.tsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Dropdown from "../commons/DropDown";
 import Steps from "./utils/Steps";
+import { Button } from "../commons/Button";
 
 const BookingPanel = () => {
   const [branch, setBranch] = useState("");
   return (
-    <section>
-      <h1 className="font-roboto text-2xl font-bold mt-5 mb-5 text-start">
-        Hacer una reserva
-      </h1>
+    <section className="bg-grey1 h-screen">
+      <div className="flex flex-col ml-[10rem]">
+        <h1 className="font-roboto text-2xl font-bold mt-5 mb-5 text-start">
+          Hacer una reserva
+        </h1>
+      </div>
       <div className=" flex justify-center ">
-        <div className="flex flex-col border border-black w-679 h-362 mr-8 p-7">
-          <div className="flex justify-start flex-col">
-            <h1 className="font-roboto text-2xl font-bold mt-7 mb-1 text-start ">
+        <div className="flex flex-col rounded-lg w-679 h-362 mr-8 py-8 px-10 bg-white">
+          <div className="flex justify-start flex-col mt-2 ">
+            <h1 className="font-roboto text-2xl font-bold  mb-1 text-start ">
               Reserva
             </h1>
             <h2 className="block text-sm text-black font-roboto">
               Seleccioná tu sucursal
             </h2>
           </div>
-          <div className="flex w-full border border-black flex-col">
+          <div className="flex w-full  flex-col mt-6">
             <Steps
               icon="1"
               text=""
@@ -27,12 +30,15 @@ const BookingPanel = () => {
               textColor="text-violet"
             />
           </div>
-          <div className="flex w-full flex-col mt-5">
+          <div className="flex w-full flex-col mt-5 font-roboto">
             <h2>Sucursal</h2>
             <Dropdown />
           </div>
+          <div className="flex justify-start mt-6">
+            <Button />
+          </div>
         </div>
-        <div className="border border-black w-457 ml-3 p-5">Calendario</div>
+        <div className=" w-457 ml-3 p-5 rounded-lg bg-white"></div>
       </div>
     </section>
   );
--- src/commons/DropDown.tsx
+++ src/commons/DropDown.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Dropdown() {
   return (
-    <div className="relative w-full lg:max-w-sm">
-      <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-        <option>ReactJS Dropdown</option>
-        <option>Laravel 9 with React</option>
-        <option>React with Tailwind CSS</option>
-        <option>React With Headless UI</option>
+    <div className="relative w-full border border-grey3 rounded-lg ">
+      <select className="w-full p-2.5 border rounded-lg shadow-sm  focus:border-indigo-600 font-roboto">
+        <option>Villa crespo</option>
+        <option>Burzaco</option>
+        <option>Ituzaingo</option>
+        <option>Tropitango</option>
       </select>
     </div>
   );
